873841cebaab110d9a2f2bcfd91e2acf66678ad4
zhufuzz <zhufuzz@hotmail.com>
2017-01-16 20:22:07 -0500

    gfh

---
+++ leetlint/src/lintcode/_2BinarySearch/MaximumAverageSubarray.java
@@ -0,0 +1,78 @@
+package lintcode._2BinarySearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by tzh on 1/15/17.
+ */
+public class MaximumAverageSubarray {
+    public double maxAverage(int[] nums, int k) {
+        // Write your code here
+        double l = Integer.MAX_VALUE, r = Integer.MIN_VALUE;
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < l)
+                l = nums[i];
+            if (nums[i] > r)
+                r = nums[i];
+        }
+        int whileCount = 0;
+        int breakCount = 0;
+        double[] sum = new double[n + 1];
+        sum[0] = 0;
+        while (r - l >= 1e-1) {
+            whileCount++;
+            double mid = (l + r) / 2.0;
+
+            double min_pre = 0;
+            boolean check = false;
+            for (int i = 1; i <= n; ++i) {
+                sum[i] = sum[i - 1] + nums[i - 1] - mid;
+                if (i >= k && sum[i] - min_pre >= 0) {
+                    check = true;
+                    System.out.println("Break");
+                    breakCount++;
+                    break;
+                }
+                if (i >= k)
+                    min_pre = Math.min(min_pre, sum[i - k + 1]);
+                    System.out.println("update");
+
+            }
+            if (check)
+                l = mid;
+            else
+                r = mid;
+        }
+        //System.out.println(l);
+        //System.out.println(r);
+        System.out.println(whileCount);
+        System.out.println("Breakcount" + breakCount);
+        return l;
+    }
+
+    @Test
+    public void test01() {
+        int[] nums = {1, 12, -5, -6, 50, 3};
+
+        int k = 3;
+        System.out.print(maxAverage(nums, k));
+    }
+
+    @Test
+    public void test02 () {
+        int[] nums = {    -2147483648,-2147483648,-2147483648,-2147483648};
+        int k = 2;
+        //System.out.print(maxAverage(nums, k));
+        Assert.assertTrue(maxAverage(nums, k) == -2147483648);
+    }
+    @Test
+    public void testMinInt() {
+        System.out.print(1e-0);
+    }
+
+
+
+
+}
---
+++ leetlint/src/lintcode/_2BinarySearch/_349_Intersection_of_Two_Arrays.java
@@ -0,0 +1,99 @@
+package lintcode._2BinarySearch;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Created by tzh on 1/15/17.
+ */
+public class _349_Intersection_of_Two_Arrays {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int[]  result = new int[0];
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0) {
+            return result;
+        }
+        ArrayList arrayList = new ArrayList();
+        Arrays.sort(nums1);
+        for (int i : nums2) {
+            ResultType resultType = helper(nums1, i);
+            System.out.println(resultType);
+            if (!resultType.exist) {
+                continue;
+            }
+            arrayList.add(i);
+            /*if (!arrayList.contains(i)) {
+                arrayList.add(i);
+            }*/
+
+        }
+        Collections.sort(arrayList);
+        result = new int[arrayList.size()];
+        for(int i = 0; i < arrayList.size(); i++) {
+            result[i] = (Integer) arrayList.get(i);
+            System.out.println(result[i]);
+        }
+
+        return result;
+
+
+
+
+    }
+    private class ResultType{
+        private boolean exist = false;
+        private int value;
+        private ResultType(boolean exist, int value) {
+            this.exist = exist;
+            this.value = value;
+        }
+        public boolean getExist() {
+            return exist;
+        }
+        public void setExist(boolean exist) {
+            this.exist = exist;
+        }
+        public int getValue() {
+            return value;
+        }
+        public void setValue(int value) {
+            this.value = value;
+        }
+        @Override
+        public String toString() {
+            return "" + exist + " " + value;
+        }
+    }
+    private ResultType helper(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length  - 1;
+        while(start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                end = mid;
+            } else if (nums[mid] > target) {
+                end =  mid;
+            } else {
+                start = mid;
+            }
+        }
+        if (nums[start] == target) {
+            return new ResultType(true, nums[start]);
+        }
+        if (nums[end] == target) {
+            return new ResultType(true, nums[end]);
+        }
+        return new ResultType(false, 0);
+    }
+
+    @Test
+    public  void test01() {
+        int[] nums1 = {1, 2, 3};
+        int[] nums2 = {1,2,3};
+        int[] result = intersection(nums1, nums2);
+        for (int i :result) {
+            System.out.println(i);
+        }
+    }
+}
