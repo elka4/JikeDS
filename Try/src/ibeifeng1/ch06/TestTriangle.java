183171a2d03e6a6076526a297fc6fb529033b141
tianhui <zhufuzz@hotmail.com>
2017-01-24 11:17:09 -0500

    dd

--- Try/src/ibeifeng1/ch06/TestTriangle.java
+++
@@ -1,8 +0,0 @@
-package ibeifeng1.ch06;
-
-public class TestTriangle {
-	public static void main(String[] args) {
-		System.out.println(Triangle.getNumber(500));
-		System.out.println(Triangle.getNumberByRecursion(500));
-	}
-}
