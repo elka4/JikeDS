c07c45c53c9114752a93fabb4f653ff26c9306bf
tianhui <zhufuzz@hotmail.com>
2017-03-01 11:40:57 -0500

    SS

--- leetlint/src/ch9J/chapter3_BianryTree/_09Binary_Tree_Maximum_Path_Sum_1.java
+++ leetlint/src/ch9J/chapter3_BianryTree/_09Binary_Tree_Maximum_Path_Sum_1.java
@@ -1,38 +1,38 @@
 package ch9J.chapter3_BianryTree;
 
 public class _09Binary_Tree_Maximum_Path_Sum_1 {
-private class ResultType {
-    // singlePath: 从root往下走到任意点的最大路径，这条路径可以不包含任何点
-    // maxPath: 从树中任意到任意点的最大路径，这条路径至少包含一个点
-    int singlePath, maxPath; 
-    ResultType(int singlePath, int maxPath) {
-        this.singlePath = singlePath;
-        this.maxPath = maxPath;
+    private class ResultType {
+        // singlePath: 从root往下走到任意点的最大路径，这条路径可以不包含任何点
+        // maxPath: 从树中任意到任意点的最大路径，这条路径至少包含一个点
+        int singlePath, maxPath;
+        ResultType(int singlePath, int maxPath) {
+            this.singlePath = singlePath;
+            this.maxPath = maxPath;
+        }
     }
-}
 
-public int maxPathSum(TreeNode root) {
-    	ResultType result = helper(root);
-    	return result.maxPath;
-}
-
-private ResultType helper(TreeNode root) {
-    if (root == null) {
-        return new ResultType(0, Integer.MIN_VALUE);
+    public int maxPathSum(TreeNode root) {
+            ResultType result = helper(root);
+            return result.maxPath;
     }
-    // Divide
-    ResultType left = helper(root.left);
-    ResultType right = helper(root.right);
 
-    // Conquer
-    int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
-    singlePath = Math.max(singlePath, 0);
+    private ResultType helper(TreeNode root) {
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE);
+        }
+        // Divide
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
 
-    int maxPath = Math.max(left.maxPath, right.maxPath);
-    maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+        // Conquer
+        int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
+        singlePath = Math.max(singlePath, 0);
 
-    return new ResultType(singlePath, maxPath);
-}
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+
+        return new ResultType(singlePath, maxPath);
+    }
 }
 
 /*Given a binary tree, find the maximum path sum.
--- leetlint/src/ch9J/chapter3_BianryTree/_09Binary_Tree_Maximum_Path_Sum_2.java
+++ leetlint/src/ch9J/chapter3_BianryTree/_09Binary_Tree_Maximum_Path_Sum_2.java
@@ -3,24 +3,24 @@ package ch9J.chapter3_BianryTree;
 //Version 2:
 //SinglePath也定义为，至少包含一个点。
 class _09Binary_Tree_Maximum_Path_Sum_2 {
- /**
-  * @param root: The root of binary tree.
-  * @return: An integer.
-  */
- private class ResultType {
+    /**
+    * @param root: The root of binary tree.
+    * @return: An integer.
+    */
+    private class ResultType {
      int singlePath, maxPath;
      ResultType(int singlePath, int maxPath) {
          this.singlePath = singlePath;
          this.maxPath = maxPath;
      }
- }
- 
- public int maxPathSum(TreeNode root) {
+    }
+
+    public int maxPathSum(TreeNode root) {
      ResultType result = helper(root);
      return result.maxPath;
- }
- 
- private ResultType helper(TreeNode root) {
+    }
+
+    private ResultType helper(TreeNode root) {
      if (root == null) {
          return new ResultType(Integer.MIN_VALUE, Integer.MIN_VALUE);
      }
@@ -34,9 +34,9 @@ class _09Binary_Tree_Maximum_Path_Sum_2 {
 
      int maxPath = Math.max(left.maxPath, right.maxPath);
      maxPath = Math.max(maxPath,
-                        Math.max(left.singlePath, 0) + 
+                        Math.max(left.singlePath, 0) +
                         Math.max(right.singlePath, 0) + root.val);
 
      return new ResultType(singlePath, maxPath);
- }
+    }
 }
