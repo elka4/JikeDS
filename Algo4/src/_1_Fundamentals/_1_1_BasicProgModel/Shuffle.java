0b249d837a7e642ce28c33abba3f454ce5334c6b
tianhuizhu <zhufuzz@hotmail.com>
2017-06-22 19:47:20 -0400

    init on new mac

--- Algo4/src/_1_Fundamentals/_1_1_BasicProgModel/Shuffle.java
+++ Algo4/src/_1_Fundamentals/_1_1_BasicProgModel/Shuffle.java
@@ -13,7 +13,7 @@ import StdLib.StdOut;
  *  the assumption that Math.random() generates independent and
  *  uniformly distributed numbers between 0 and 1.
  *
- *  % more cards.txt
+ *  % java RandomSeq 5 100.0 200.0
  *  2C 3C 4C 5C 6C 7C 8C 9C 10C JC QC KC AC
  *  2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD AD
  *  2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH AH
