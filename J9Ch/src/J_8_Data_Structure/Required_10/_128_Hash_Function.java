5bdf86054366f4266b224bd9728f5f80721bced8
tianhuizhu <zhufuzz@hotmail.com>
2017-07-10 21:36:39 -0400

    init on new mac

---
+++ J9Ch/src/J_8_Data_Structure/Required_10/_128_Hash_Function.java
@@ -0,0 +1,19 @@
+package J_8_Data_Structure.Required_10;
+import java.util.*;import lib.ListNode;
+/** 128 Hash Function
+ * Easy
+
+ * Created by tianhuizhu on 6/28/17.
+ */
+public class _128_Hash_Function {
+
+    class Solution {
+        public int hashCode(char[] key,int HASH_SIZE) {
+            long ans = 0;
+            for(int i = 0; i < key.length;i++) {
+                ans = (ans * 33 + (int)(key[i])) % HASH_SIZE;
+            }
+            return (int)ans;
+        }
+    };
+}
---
+++ J9Ch/src/lib/ListNode.java
@@ -0,0 +1,10 @@
+package lib;
+
+/**
+ * Created by tianhuizhu on 6/28/17.
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int x) { val = x; }
+}
