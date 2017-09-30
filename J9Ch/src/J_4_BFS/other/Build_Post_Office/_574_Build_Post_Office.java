91f997a3c5d7c7430451d119be2c59257c5a1841
tianhuizhu <zhufuzz@hotmail.com>
2017-09-30 09:01:17 -0400

    init on new mac

--- J9Ch/src/J_4_BFS/other/Build_Post_Office/_574_Build_Post_Office.java
+++ J9Ch/src/J_4_BFS/other/Build_Post_Office/_574_Build_Post_Office.java
@@ -1,5 +1,7 @@
 package J_4_BFS.other.Build_Post_Office;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -79,6 +81,14 @@ public class _574_Build_Post_Office {
             index = l;
         return sum.get(n) - sum.get(index + 1) - pos * (n - index - 1) +
                 (index + 1) * pos - sum.get(index + 1);
+    }
+
+    @Test
+    public void test01(){
+        int[][] grid = {{0 ,1 ,0},
+                        {1, 1, 0},
+                        {1, 0, 0}  };
+        System.out.println(shortestDistance(grid));
     }
 
 
@@ -173,5 +183,18 @@ public class _574_Build_Post_Office {
         }
         return false;
     }
+/////////////////////////////////////////////////////////////////////////////
 
+
+/////////////////////////////////////////////////////////////////////////////
 }
+/*
+Given a 2D grid, each cell is either an house 1 or empty 0 (the number zero, one), find the place to build a post office, the distance that post office to all the house sum is smallest. Return the smallest distance. Return -1 if it is not possible.
+Notice
+You can pass through house and empty.
+You only build post office on an empty.
+Example
+Given a grid:
+0 1 0 0 1 0 1 1 0 1 0 0
+return 6. (Placing a post office at (1,1), the distance that post office to all the house sum is smallest.)
+ */
