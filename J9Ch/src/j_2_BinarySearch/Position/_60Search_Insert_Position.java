9d034d9ddf156e253ff40c4c7ce89f70c615331d
bruce <zhufuzz@hotmail.com>
2017-11-03 09:50:18 -0400

    back

---
+++ J9Ch/src/j_2_BinarySearch/Position/_60Search_Insert_Position.java
@@ -0,0 +1,133 @@
+package j_2_BinarySearch.Position;
+
+//version 1: find the first position >= target
+//So that is similar to find first position.
+//So at the end, try >=nums[start] first,
+//then try >= nums[end] second
+//at last give end + 1
+
+// Search Insert Position
+// version 1: find the first position >= target
+
+//  35. Search Insert Position
+
+public class _60Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0, end = nums.length - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (nums[start] >= target) {
+            return start;
+        } else if (nums[end] >= target) {
+            return end;
+        } else {
+            return end + 1;
+        }
+    }
+
+//version 2: find the last position < target, return +1， 
+//要特判一下target小于所有数组里面的元素
+
+class Search_Insert_Position2 {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+           return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
+
+        if (target < nums[0]) {
+           return 0;
+        }
+        // find the last number less than target
+        while (start + 1 < end) {
+           mid = start + (end - start) / 2;
+           if (nums[mid] == target) {
+               return mid;
+           } else if (nums[mid] < target) {
+               start = mid;
+           } else {
+               end = mid;
+           }
+        }
+
+        if (nums[end] == target) {
+           return end;
+        }
+        if (nums[end] < target) {
+           return end + 1;
+        }
+        if (nums[start] == target) {
+           return start;
+        }
+        return start + 1;
+    }
+}
+
+ 
+ //this is the code in Ch9 2016 summer.
+    public int searchInseart2(int[] nums, int target){
+        if (nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int start = 0, end = nums.length - 1;
+
+        while(start + 1 < end){
+             int mid = start + (end - start) / 2;
+             if(nums[mid] >= target){
+                 end = mid;
+             }else {
+                 start = mid;
+             }
+        }
+        if(nums[start] == target){
+            return start;
+        }
+        if(nums[end] >= target){
+            return end;
+        }
+        return nums.length;
+    }
+}
+
+
+
+/*
+Given a sorted array and a target value,  return the 
+index if the target is found. If not, return the index
+ where it would be if it were inserted in order.
+
+You may assume NO duplicates in the array.
+
+Have you met this question in a real interview? Yes
+Example
+[1,3,5,6], 5 → 2
+
+[1,3,5,6], 2 → 1
+
+[1,3,5,6], 7 → 4
+
+[1,3,5,6], 0 → 0
+
+Challenge 
+O(log(n)) time
+
+Tags 
+Binary Search Sorted numsrray numsrray
+ * 
+ * */
---
+++ J9Ch/src/j_2_BinarySearch/Rotated/_62Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,114 @@
+package j_2_BinarySearch.Rotated;
+
+// Search in Rotated Sorted numsrray
+//
+//  33. Search in Rotated Sorted numsrray
+
+public class _62Search_in_Rotated_Sorted_Array {
+	 public int search(int[] nums, int target) {
+	    if (nums == null || nums.length == 0) {
+	        return -1;
+	    }
+	
+	    int start = 0;
+	    int end = nums.length - 1;
+	    int mid;
+	    
+	    while (start + 1 < end) {
+	        mid = start + (end - start) / 2;
+	        if (nums[mid] == target) {
+	            return mid;
+	        }
+	        
+	        // USE STnumsRT TO COMPnumsRE !!!!
+	        if (nums[start] < nums[mid]) {
+	            // situation 1, red line
+	            if (nums[start] <= target && target <= nums[mid]) {
+	                end = mid;
+	            } else {
+	                start = mid;
+	            }
+	        } else {
+	            // situation 2, green line
+	            if (nums[mid] <= target && target <= nums[end]) {
+	                start = mid;
+	            } else {
+	                end = mid;
+	            }
+	        }
+	    } // while
+	    
+	    if (nums[start] == target) {
+	        return start;
+	    }
+	    if (nums[end] == target) {
+	        return end;
+	    }
+	    return -1;
+	  }
+
+
+	public int search_mine1(int[] nums, int target) {
+		// write your code here
+		if (nums == null || nums.length == 0) {
+			return -1;
+		}
+		int start = 0;
+		int end = nums.length - 1;
+
+		while(start + 1 < end){
+			int mid = start + (end - start) / 2;
+
+			if (nums[mid] > nums[start]){
+				if(nums[start] <= target && target <= nums[mid]){
+					end = mid;
+				} else {
+					start = mid;
+				}
+			} else if (nums[mid] < nums[start]){
+				if(nums[mid] <= target && target <= nums[end]){
+					start = mid;
+				} else {
+					end = mid;
+				}
+			} else {
+				start = mid; // end also numsC
+			}
+		}
+		if (nums[start] == target){
+			return start;
+		} else if (nums[end] == target){
+			return end;
+		} else {
+			return -1;
+		}
+	}
+}
+
+/*
+ * Suppose a sorted array is rotated at some pivot 
+ * unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the 
+array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Have you met this question in a real interview? Yes
+Example
+For [4, 5, 1, 2, 3] and target=1, return 2.
+
+For [4, 5, 1, 2, 3] and target=0, return -1.
+
+Challenge 
+O(logN) time
+
+Tags 
+Binary Search Sorted numsrray numsrray LinkedIn Uber Facebook
+Related Problems 
+Medium Search in Rotated Sorted numsrray II 39 %
+Easy Search a 2D Matrix 27 %
+ * 
+ * */
