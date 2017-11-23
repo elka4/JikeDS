6bd2d03d3a8622382e8549b346f27ba92329a7da
bruce <zhufuzz@hotmail.com>
2017-11-23 14:34:08 -0500

    back

--- J9Ch/src/HF/HF4_Search_DFS/_4AddOperators.java
+++ J9Ch/src/HF/HF4_Search_DFS/_4AddOperators.java
@@ -108,10 +108,12 @@ public class _4AddOperators {
 Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
 
 Have you met this question in a real interview? Yes
+----------------------------------------------------
 Example
 "123", 6 -> ["1+2+3", "1*2*3"]
 "232", 8 -> ["2*3+2", "2+3*2"]
 "105", 5 -> ["1*0+5","10-5"]
 "00", 0 -> ["0+0", "0-0", "0*0"]
 "3456237490", 9191 -> []
+----------------------------------------------------
  */
