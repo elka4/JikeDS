217b357f551b6321ba0ecd66d38a0aa0a4fc66aa
zhufuzz <zhufuzz@hotmail.com>
2018-02-08 16:49:22 -0500

    init on macbook pro

---
+++ Leet_Ed_C/src/ListNode.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Edward on 25/07/2017.
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
